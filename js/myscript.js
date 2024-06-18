1d9816e6581c98f3eaa3fa0e13d516ca41cbb50a
Samgrass <samuelegrassetti01@gmail.com>
2024-06-18 17:24:19 +0200

    creo l'elemento card con i dati degli oggetti

--- js/myscript.js
+++ js/myscript.js
@@ -1,3 +1,5 @@
+// creo variabile pe rpoi inserire gli oggetti nell'html
+const containerTeam = document.getElementById("container-team");
 // creare un array di oggetti per rappresentare i membri del team. Ogni membro è caratterizzato dalle seguenti informazioni: nome, ruolo e foto.
 const team = [
     {
@@ -40,4 +42,15 @@ const team = [
     let roleTeam = elementTeam.role;
     let imgTeam = elementTeam.img;
     console.log(nameTeam, roleTeam, imgTeam);
+
+    let cardTeam = 
+                    `<div class="col">
+                        <div>
+                            <img src="img/${imgTeam}" alt="${nameTeam}">
+                        </div>
+                        <div>${nameTeam}</div>
+                        <div>${roleTeam}</div>
+                    </div>`
+
+    containerTeam.innerHTML += cardTeam
  }
