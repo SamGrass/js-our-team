4904461134e870991b9e85a5e2c849447b062186
Samgrass <samuelegrassetti01@gmail.com>
2024-06-18 17:03:08 +0200

    aggiungo elementi + commenti js + creo array team

---
+++ js/myscript.js
@@ -0,0 +1,35 @@
+// creare un array di oggetti per rappresentare i membri del team. Ogni membro è caratterizzato dalle seguenti informazioni: nome, ruolo e foto.
+const team = [
+    {
+        "name": 'Wayne Barnett',
+        "role": 'Founder and CEO',
+        "img": 'wayne-barnett-founder-ceo.jpg'
+    },
+    {
+        "name": 'Angela Caroll',
+        "role": 'Chief Editor',
+        "img": 'angela-caroll-chief-editor.jpg'
+    },
+    {
+        "name": 'Walter Gordon',
+        "role": 'Office Manager',
+        "img": 'walter-gordon-office-manager.jpg'
+    },
+    {
+        "name": 'Angela Lopez',
+        "role": 'Social Media Manager',
+        "img": 'angela-lopez-social-media-manager.jpg'
+    },
+    {
+        "name": 'Scott Estrada',
+        "role": 'Developer',
+        "img": 'scott-estrada-developer.jpg'
+    },
+    {
+        "name": 'Barbara Ramos',
+        "role": 'Graphic Designer',
+        "img": 'barbara-ramos-graphic-designer.jpg'
+    }
+]
+// stampo le informazioni in console
+// creo un ciclo per estrarre le informazioni
